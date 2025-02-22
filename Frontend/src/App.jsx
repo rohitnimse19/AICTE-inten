d66d095ba62b191f087706541ff5140eb49f6aac
rohitnimse19 <nimserohit75@gmail.com>
2025-02-22 23:54:18 +0530

    First Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Card from './component/Card'
+
+function App() {
+  return (
+   <><Navbar/>
+   <Card/>   
+   </> 
+  )
+}
+
+export default App
